0fda19340f5e78bcca1f47d98847866098b86755
wycliff324-code <rasg324@gmail.com>
2025-09-23 08:51:43 +0300

    Add files via upload

---
+++ Height phone number and balance.c
@@ -0,0 +1,15 @@
+// sipmple Cprogram
+/*
+Name: wycliff mutharimi
+Reg No: CT101/G/26561/25
+Date: 18/9/2025
+Describtion: hello world
+*/
+#include<stdio.h>
+void main(){
+  int a;
+  scanf("%d", &a);
+  printf("Enter height: 167:\n");
+  printf("Enter bank balance: 1000:\n");
+  printf("Enter phone No:  0705731314");
+}
---
+++ units for this semester.c
@@ -0,0 +1,19 @@
+/*
+Name: Wycliff mutharimi
+Reg No: CT101/G/26561/25
+Date:
+Description: Units for this semester
+*/
+#include<stdio.h>
+//main function
+1nt main(){
+printf("unit 1:SPC 2101 INTRODUCTION TO PRONGRAMING AN DPROBLEM SOLVING:\n");
+printf("unit 2:SPC 2102 INTRODUCTION TO COMPUTER ARCHTECTURE:\n");
+printf("unit 3:SPH 2107 ANALOGUE ELOCTRONICS:\n");
+printf("unit 4:SPM 2100 DICRETE MATHEMATICS:\n");
+printf("unit 5:SPM 2104 MATHEMATICS FOR SCIENCE:\n:");
+printf("unit 6:SPH 2101 PHYSICS:\n");
+printf("unit 7:UCU 2105 FUNDAMENTALS OF ICT:\n");
+printf("unit 8:UCU 2106 COMMUNICATION SKILLS")
+return 0;
+}
