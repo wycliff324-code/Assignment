09ea0db600626cfe7143beb40e4a9c31cd98f5a8
wycliff324-code <rasg324@gmail.com>
2025-09-24 20:33:31 +0300

    Height phone number and balance. c

--- Height phone number and balance.c
+++ Height phone number and balance.c
@@ -1,15 +1,22 @@
-// sipmple Cprogram
 /*
 Name: wycliff mutharimi
 Reg No: CT101/G/26561/25
 Date: 18/9/2025
-Describtion: hello world
+Describtion: 
 */
 #include<stdio.h>
-void main(){
-  int a;
-  scanf("%d", &a);
-  printf("Enter height: 167:\n");
-  printf("Enter bank balance: 1000:\n");
-  printf("Enter phone No:  0705731314");
+int main(){
+  int height;
+  double bankbalance;
+  char phonenumber[20];
+  printf("enter your height: ");
+  scanf("%d", &height);
+  printf("enter your bankbalance:");
+  scanf("%lf",&bankbalance:);
+  printf("enter your phonenumber":);
+  scanf("%s",phonenumber)
+  printf("your height is %dmetres\n",height);
+  printf("your bankbalance is Ksh %.2lf\n",bankbalance);
+  printf("your phonenumber is %s\n"phonenumber);
+  return 0;
 }
