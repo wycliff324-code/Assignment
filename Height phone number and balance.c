0435508f7f0f48fe788d010cbdba31de7d30cead
wycliff324-code <rasg324@gmail.com>
2025-09-24 20:39:23 +0300

    Update Height phone number and balance. c

--- Height phone number and balance.c
+++ Height phone number and balance.c
@@ -9,12 +9,16 @@ int main(){
   int height;
   double bankbalance;
   char phonenumber[20];
+  
   printf("enter your height: ");
   scanf("%d", &height);
+  
   printf("enter your bankbalance:");
   scanf("%lf",&bankbalance:);
+  
   printf("enter your phonenumber":);
-  scanf("%s",phonenumber)
+  scanf("%s",phonenumber:);
+  // display all together
   printf("your height is %dmetres\n",height);
   printf("your bankbalance is Ksh %.2lf\n",bankbalance);
   printf("your phonenumber is %s\n"phonenumber);
